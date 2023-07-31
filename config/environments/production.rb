3086dc873289b8c7ad763409d88b51423caff2d2
talk2awais <muhammad.awais.77831@gmail.com>
2023-07-31 20:11:58 +0500

    Adding devise and admin_user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,24 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+  # config/environments/production.rb
+    # Other configuration settings...
+
+=begin
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.example.com',
+      port: 587,
+      domain: 'example.com',
+      user_name: 'your_username',
+      password: 'your_password',
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+=end
+
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
